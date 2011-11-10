41b8c0ce8f44f7692136c16e1703778caf63616b
Felipe Travassosfelipe.silva.travassos <felipe.silva.travassos@localhost>
2011-11-10 15:09:12 -0200

    Class eventos

---
+++ iCall_C++/Evento.cpp
@@ -0,0 +1,82 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+class Evento{
+      public:
+             //Nome evento, Nome tema evento, Apresentador
+             static void novoEvento(string, string, string);
+             static void listaUsuarios (string);
+             
+             static string getNomeEvento();
+             static string getNomeApresentador();
+             static string getTemaEvento();
+             static string getUsuarios();
+             
+             static void setNomeEvento(string);
+             static void setNomeApresentador(string);
+             static void setTemaEvento(string);
+             static void setListaUsuarios (string);
+};
+
+void Evento::novoEvento(string nomeEvento, string temaEvento, string apresentador){
+       
+}
+
+void Evento::listaUsuarios (string usuarios){
+     
+}
+
+string Evento::getNomeEvento(){
+  
+}
+
+string Evento::getNomeApresentador(){
+       
+}
+
+string Evento::getTemaEvento(){
+       
+}
+
+string Evento::getUsuarios(){
+       
+}
+
+void Evento::setNomeEvento(string nomeEvento){
+
+}
+
+void Evento::setNomeApresentador(string nomeApresentador){
+
+}
+
+void Evento::setTemaEvento(string temaEvento){
+
+}
+
+void Evento::setListaUsuarios(string listaUsuarios){
+
+}
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
