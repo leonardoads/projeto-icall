b95c4ec10b3b03a1236c8c8a9d7481519bd7f27a
Felipe <felipe.silva.travassos@gmail.com>
2012-12-10 22:10:02 -0300

    Criada uma classe para tratar do banco de dados

--- trunk/src/InterfaceGrafica/Login.java
+++ trunk/src/InterfaceGrafica/Login.java
@@ -1,11 +1,9 @@
 package InterfaceGrafica;
 
-import java.awt.BorderLayout;
+import gerenciarDB.DBConnection;
+
 import java.awt.Color;
 import java.awt.Component;
-import java.awt.Event;
-import java.awt.GridLayout;
-import java.awt.RenderingHints.Key;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -26,6 +24,7 @@ import javax.swing.JTextField;
 import classes.LerArquivo;
 
 public class Login extends Main implements ActionListener{
+	
 	JPanel painelFinal = new JPanel();
 	final String systemSeparator = java.io.File.separator;
 	final String ICALLPATH = System.getProperty("user.home") + systemSeparator
